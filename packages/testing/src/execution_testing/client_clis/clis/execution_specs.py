05fd9f236ef8c10c10cb10e2f1d35d197169a313
Guruprasad Kamath <guru241987@gmail.com>
2026-05-26 12:04:00 +0200

    refactor(spec-tools): final clean up

--- packages/testing/src/execution_testing/client_clis/clis/execution_specs.py
+++ packages/testing/src/execution_testing/client_clis/clis/execution_specs.py
@@ -8,6 +8,8 @@ from typing import Any, ClassVar, Dict, Optional
 
 import ethereum
 from ethereum_spec_tools.evm_tools.t8n import T8N, ForkCache
+from ethereum_spec_tools.evm_tools.t8n.evm_trace.eip3155 import Eip3155Tracer
+from ethereum_spec_tools.evm_tools.t8n.evm_trace.group import GroupTracer
 from ethereum_spec_tools.evm_tools.utils import get_supported_forks
 from typing_extensions import override
 
@@ -84,13 +86,6 @@ class ExecutionSpecsTransitionTool(TransitionTool):
 
         tracers = None
         if self.trace:
-            from ethereum_spec_tools.evm_tools.t8n.evm_trace.eip3155 import (
-                Eip3155Tracer,
-            )
-            from ethereum_spec_tools.evm_tools.t8n.evm_trace.group import (
-                GroupTracer,
-            )
-
             tracers = GroupTracer()
             tracers.add(
                 Eip3155Tracer(
